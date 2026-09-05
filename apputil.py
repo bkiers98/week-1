1e8c25b1f929d2c568b146f35d10e1fb9c4cf16d
bkiers98 <167789286+bkiers98@users.noreply.github.com>
2026-09-05 09:36:47 -0400

    Complete first exercise and add to apputil.py

--- apputil.py
+++ apputil.py
@@ -1,3 +1,17 @@
 
 
 # add code below ...
+def palindrome(word):
+    '''
+        This function receives a string and returns True or False to indicate whether or not it is a palindrome.
+    '''
+
+    punctuation = '.,?! '
+    for char in punctuation:
+        word = word.replace(char, "")
+    word = word.lower()
+    for letter in range(len(word)):
+        if word[letter] != word[-(letter + 1)]:
+            return False
+    return True
+
